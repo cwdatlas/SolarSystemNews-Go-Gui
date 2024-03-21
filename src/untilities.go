66bef2460ea2e7c69598257a1a72ca3669d7af37
cwdatlas <ascott@carroll.edu>
2024-03-21 07:19:52 -0600

    added comments

--- src/untilities.go
+++ src/untilities.go
@@ -11,76 +11,116 @@ import (
 	"gioui.org/widget/material"
 )
 
+/*
+	Author Aidan Scott
+	Utilities holds many nice to have separate functions so config can be called once in the name of DRY
+*/
+// config for button margins
 var buttonMargins = layout.Inset{
 	Top:    unit.Dp(15),
 	Bottom: unit.Dp(15),
 	Right:  unit.Dp(35),
 	Left:   unit.Dp(35),
 }
+
+// config for rows, used in article button display
 var rows = layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceEvenly}
 
+// return margin config
 func getButtonMargins() layout.Inset {
 	return buttonMargins
 }
 
+// return Rigid that displays a button and manages its event
 func getButtonClickable(btn *widget.Clickable, article string, name string) layout.FlexChild {
 	th := material.NewTheme()
 	articleButton := layout.Rigid(
 		func(gtx layout.Context) layout.Dimensions {
+			// sets margin config
 			margins := getButtonMargins()
 			return margins.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				// if button is clicked then display new window with article text
 				for btn.Clicked(gtx) {
 					createInfoWindow(article, name)
 				}
+				// set button then return dims
 				btn := material.Button(th, btn, name)
 				return btn.Layout(gtx)
 			})
 		})
+	// return flex child for visualization
 	return articleButton
 }
 
+/*
+	creates a new window of specific size with the given params
+	article: is the entire string related to the article
+	name: is the articles name
+*/
+
 func createInfoWindow(article string, name string) {
 	go func() {
+		// set window
 		w := app.NewWindow(
+			// set title to name
 			app.Title(name),
+			// set size
 			app.Size(unit.Dp(1350), unit.Dp(200)),
 		)
+		// if error stop window, close it
 		if err := infoPanelLoop(w, article); err != nil {
 			panic(err)
 		}
 	}()
 
 }
+
+/*
+'draw' but for the info panel
+This function provides the structure for elements and displays the article and a button to exit
+w: *app.Window: used to tell what window is being added to
+article: full string of the article
+*/
 func infoPanelLoop(w *app.Window, article string) error {
+	// setting dependents
 	th := material.NewTheme()
-	okButton := new(widget.Clickable)
 	var ops op.Ops
+	// creating ok button
+	okButton := new(widget.Clickable)
 	for {
+		// setting up switch so app can be drawn when events occur
 		switch env := w.NextEvent().(type) {
 		case app.FrameEvent:
+			// set up context
 			gtx := app.NewContext(&ops, env)
+			// set layout variables of the layout
 			layout.Flex{
-				Axis:    layout.Vertical,
+				Axis:    layout.Vertical, // elements will be displayed vertically
 				Spacing: layout.SpaceStart,
 			}.Layout(gtx,
-
+				// Rigid to display the article
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					// set, align then return article's dims
 					article := material.H6(th, article)
 					article.Alignment = text.Middle
 					return article.Layout(gtx)
 				},
 				),
+				// Rigid to display button
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 					// Layout the "OK" button at the bottom
 					for okButton.Clicked(gtx) {
 						w.Perform(system2.ActionClose) // This will close the window
 					}
+					// create physical button and return dims
 					btn := material.Button(th, okButton, "Close")
 					return btn.Layout(gtx)
 				}),
 			)
+			// draw frame
 			env.Frame(gtx.Ops)
 		case app.DestroyEvent:
+			// on destroy shut dow process
 			return env.Err
 
 		}
