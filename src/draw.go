2f997966bb03f9258ce9bfec0acbefead98d9ed5
cwdatlas <ascott@carroll.edu>
2024-03-21 06:37:23 -0600

    created drawing logic for majority of program

--- src/draw.go
+++ src/draw.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"gioui.org/app"
 	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/unit"
+	"gioui.org/text"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
@@ -16,13 +15,30 @@ const (
 )
 
 func draw(w *app.Window) error {
-	// logic operators
-	newsWindow := system
 	// Window variables
 	var ops op.Ops
 	var localButton widget.Clickable
 	var systemButton widget.Clickable
 	th := material.NewTheme()
+
+	// logic operators
+	var newsWindow = system
+	sysBtns := map[string]widget.Clickable{}
+	sysArtBtns := map[string]layout.FlexChild{}
+	for k := range getSystemNews() {
+		sysBtns[k] = widget.Clickable{}
+		btn := sysBtns[k]
+		sysArtBtns[k] = getButtonClickable(&btn, getSystemNews()[k][1], getSystemNews()[k][0])
+	}
+
+	locBtns := map[string]widget.Clickable{}
+	locArtBtns := map[string]layout.FlexChild{}
+	for k := range getLocalNews() {
+		locBtns[k] = widget.Clickable{}
+		btn := locBtns[k]
+		locArtBtns[k] = getButtonClickable(&btn, getLocalNews()[k][1], getLocalNews()[k][0])
+	}
+
 	for {
 		// detect what type of event
 		switch env := w.NextEvent().(type) {
@@ -34,6 +50,14 @@ func draw(w *app.Window) error {
 				Axis:    layout.Vertical,
 				Spacing: layout.SpaceStart,
 			}.Layout(gtx,
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return getButtonMargins().Layout(gtx,
+						func(gtx layout.Context) layout.Dimensions {
+							info := material.H4(th, "Welcome to Comet News")
+							info.Alignment = text.Middle
+							return info.Layout(gtx)
+						})
+				}),
 				layout.Rigid(
 					func(gtx layout.Context) layout.Dimensions {
 						// ONE: First define margins around the button using layout.Inset ...
@@ -66,12 +90,51 @@ func draw(w *app.Window) error {
 						)
 					},
 				),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return getButtonMargins().Layout(gtx,
+						func(gtx layout.Context) layout.Dimensions {
+							info := material.H6(th, "News Articles From the Comet")
+							info.Alignment = text.Middle
+							return info.Layout(gtx)
+						})
+				}),
 				layout.Rigid(
-					layout.Spacer{Height: unit.Dp(25)}.Layout,
+					func(gtx layout.Context) layout.Dimensions {
+						dem := layout.Dimensions{}
+						if newsWindow == system {
+							dem = rows.Layout(gtx, sysArtBtns["venus"])
+						}
+						if newsWindow == local {
+							dem = rows.Layout(gtx, locArtBtns["updates"])
+						}
+						return dem
+					},
+				),
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						dem := layout.Dimensions{}
+						if newsWindow == system {
+							dem = rows.Layout(gtx, sysArtBtns["mirror"])
+						}
+						if newsWindow == local {
+							dem = rows.Layout(gtx, locArtBtns["comets"])
+						}
+						return dem
+					},
+				),
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						dem := layout.Dimensions{}
+						if newsWindow == system {
+							dem = rows.Layout(gtx, sysArtBtns["work"])
+						}
+						if newsWindow == local {
+							dem = rows.Layout(gtx, locArtBtns["carriers"])
+						}
+						return dem
+					},
 				),
 			)
-			// ...
-			fmt.Println(newsWindow)
 			env.Frame(gtx.Ops)
 		// this is sent when the application is closed
 		case app.DestroyEvent:
