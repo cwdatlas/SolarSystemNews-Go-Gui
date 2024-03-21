9d5a009b759f8e84b759859fdf68521b786ddaeb
cwdatlas <ascott@carroll.edu>
2024-03-21 07:07:15 -0600

    added comments

--- src/newsViewer.go
+++ src/newsViewer.go
@@ -7,16 +7,25 @@ import (
 	"os"
 )
 
+/*
+Author Aidan Scott
+Starts program of fictional news viewer called Passing Comet
+it is named this way because a comet 'drops' off the news
+*/
 func main() {
 	go func() {
 		// create new window
 		w := app.NewWindow(
+			// set title
 			app.Title("Passing Comet"),
+			// set size
 			app.Size(unit.Dp(600), unit.Dp(490)),
 		)
+		// if error is returned then log the error and exit
 		if err := draw(w); err != nil {
 			log.Fatal(err)
 		}
+		// exit nominally
 		os.Exit(0)
 	}()
 	app.Main()
